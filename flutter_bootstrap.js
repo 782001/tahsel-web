3a0eb7c0f5466fbe08ea9342a860f58a01c299e1
Abdalla <alwdyb782001@gmail.com>
2026-06-25 21:59:00 +0300

    feat: implement landing page structure with FAQ, pricing, and stats sections alongside localization support.

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2807440653" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3660826547" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
